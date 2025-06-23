ac064add84643371234f6e4ee1226ca37482badf
vijaynaidu111 <vijaynaidulearning@gmail.com>
2025-06-23 07:08:13 +0530

    reverse Array

---
+++ reversearray.go
@@ -0,0 +1,39 @@
+/*
+// Sample code to perform I/O:
+
+fmt.Scanf("%s", &myname)            // Reading input from STDIN
+fmt.Println("Hello", myname)        // Writing output to STDOUT
+
+// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+*/
+
+// Write your code here
+
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Printf("enter Number of elemnts")
+	fmt.Scan(&n)
+	array1 := make([]int, n)
+	//array1 := [5]int{2, 5, 9, 0, 1}
+	fmt.Printf("enter the %d numbers", n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&array1[i])
+	}
+	left := 0
+	right := len(array1) - 1
+	//fmt.Println(array1)
+
+	for left < right {
+		array1[left], array1[right] = array1[right], array1[left]
+		right--
+		left++
+	}
+	fmt.Println("output")
+	for j := 0; j < n; j++ {
+		fmt.Println(array1[j])
+	}
+}
